1c55a6d8a282a95630cdcb7ebdc0dc61048bd339
Marin <marin.basic02@gmail.com>
2019-02-22 14:02:23 +0100

    Initial init of explorer service

---
+++ explorer.go
@@ -0,0 +1,40 @@
+package walletwatcher
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// Explorer makes API calls to different endpoints depending on wallet type
+type Explorer struct {
+	client *http.Client
+}
+
+// Execute calls the API endpoint and returns the ouput
+func (e *Explorer) Execute(forWallet WalletType, address string) ([]byte, error) {
+	if e.client == nil {
+		e.client = http.DefaultClient
+	}
+
+	resp, err := e.client.Get(fmt.Sprintf("%s/%s", e.getURI(forWallet), address))
+	if err != nil {
+		return []byte{}, err
+	}
+	defer resp.Body.Close()
+
+	buff, err := ioutil.ReadAll(resp.Body)
+	return buff, err
+}
+
+// getURI returns API url for given wallet type
+func (e *Explorer) getURI(forWallet WalletType) string {
+	switch forWallet {
+	case WalletTypeBTC:
+		return ""
+	case WalletTypeETH:
+		return ""
+	}
+
+	return ""
+}
